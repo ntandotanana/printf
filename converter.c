35aded9f3eed8d46af50ce4a63b27e2bbb755913
Duduzile <celiwedudu@gmail.com>
2022-10-17 14:36:59 +0200

    Update converter.c

--- converter.c
+++ converter.c
@@ -16,8 +16,8 @@ char *convert(unsigned long int num, int base, int lowercase)
     rep = (lowercase)
 ? "0123456789abcdef"
 : "0123456789ABCDEF";
-    ptr = &buffer[49];
-    *ptr = '\0';
+  ptr = &buffer[49];
+  *ptr = '\0';
     do
     {
         *--ptr = rep[num % base];
