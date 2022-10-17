a33691092377bcc5447979306082f3370d054273
Duduzile <celiwedudu@gmail.com>
2022-10-17 09:25:08 +0200

    update print.c

--- _printf.c
+++ _printf.c
@@ -8,15 +8,14 @@
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
+
 int _printf(const char *format, ...)
 {
     int (*pfunc)(va_list, flags_t *);
     const char *p;
     va_list arguments;
     flags_t flags = {0, 0, 0};
-
     register int count = 0;
-
     va_start(arguments, format);
     if (!format || (format[0] == '%' && !format[1]))
         return (-1);
@@ -33,14 +32,14 @@ int _printf(const char *format, ...)
                 continue;
             }
             while (get_flag(*p, &flags))
-                p++;
+            p++;
             pfunc = get_print(*p);
             count += (pfunc)
-                         ? pfunc(arguments, &flags)
-                         : _printf("%%%c", *p);
+            ? pfunc(arguments, &flags)
+            : _printf("%%%c", *p);
         }
         else
-            count += _putchar(*p);
+        count += _putchar(*p);
     }
     _putchar(-1);
     va_end(arguments);
