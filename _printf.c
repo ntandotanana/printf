d7921aa5720abbce809dcea4ac3327230018c4d7
Duduzile <celiwedudu@gmail.com>
2022-10-17 10:09:17 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...);
     va_start(arguments, format);
     if (!format || (format[0] == '%' && !format[1]))
         return (-1);
-    if (format[0] == '%' && format[1] == '' && !format[2])
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
         return (-1);
     for (p = format; *p; p++)
     {
